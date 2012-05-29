616ae7847413a460a16bc3164929e495815b7e7a
Palle <palle.torsson@gmail.com>
2012-05-29 10:04:29 +0200

    sorting and search on songs

--- mysqli/public/index.php
+++ mysqli/public/index.php
@@ -4,22 +4,39 @@ include('../server/connect.php');
 $table1 = 'song';
 $table2 = 'song_artist';
 $var = '';
-$var = @$_GET['q'];
+$var = @$_GET['search'];
+$order = @$_GET['sort_type'];
+$order_type = 'song.name';
+ 
+if ($order == "artist") { 
+ 	$order_type = "artist.name";	
+	} else if ($order == "song") { 
+	$order_type = "song.name";
+	} else {
+	$order_type = "album.name"; 	
+}
 
-$stmt = $PDO->prepare("
-	SELECT song.name as track, song.id as id, artist.name as artist, artist.id as artist_id, album.name as album FROM song 
 
-	INNER JOIN song_artist
-	ON song_artist.song_id = song.id 
-	LEFT JOIN artist
-	ON artist.id = song_artist.artist_id
+$query = "SELECT song.name as track, song.id as id, GROUP_CONCAT(artist.name) as artist, artist.id as artist_id, GROUP_CONCAT(album.name) as album
+	
+	FROM song 
+	INNER JOIN artist
+	ON artist.id = song.artist_id
 
 	INNER JOIN song_album
 	ON song_album.song_id = song.id 
 	LEFT JOIN album
 	ON album.id = song_album.album_id
 
-	WHERE song.name LIKE CONCAT(:find) ");
+	WHERE song.name LIKE CONCAT(:find) 
+	OR artist.name LIKE CONCAT(:find)
+	OR album.name LIKE CONCAT(:find) 
+	GROUP BY song.name, album.name, artist.name
+	ORDER BY ".$order_type." ASC";
+
+
+$stmt = $PDO->prepare($query);
+
 
 $binds = array(':find' => "%".$var."%");
 
@@ -28,10 +45,9 @@ $binds = array(':find' => "%".$var."%");
 $stmt->execute($binds);
 $count = $stmt->rowCount();
 
+
 // Fetch and send data 
 $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC); // fetch LIMIT 1
-// print_r($result_set); 
-
 ?>
 <?php include('header.php'); ?>
 
@@ -46,7 +62,7 @@ $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC); // fetch LIMIT 1
 	   </tr>
 	   <tr>
 	     <td>Term</td>
-	     <td> <input type="text" name="q" /></td>
+	     <td> <input type="text" name="search" /></td>
 	     <td><input type="submit" name="Submit" value="Search" /></td>
 	  </form>
 	</table>
@@ -68,17 +84,19 @@ $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC); // fetch LIMIT 1
 		$i = 0; ?>
 	</div>
 	
-		<?php
-		echo "<table class=\"searchresults\">";
-		echo "<tr><th>Add</th><th>Track</th><th>Artist</th><th>Album</th></tr>";
 		
+		<table class="searchresults">
+		<tr><th>Add</th><th><a href="index.php?search=<?php echo $var ?>&sort_type=song">Song</a>
+			</th><th><a href="index.php?search=<?php echo $var ?>&sort_type=artist">Artist</a></th>
+			<th><a href="index.php?search=<?php echo $var ?>&sort_type=album">Album</a></th></tr>
+		<?php
 		while ($i < $count) {
 			echo "<tr><th>" ?>
 			<form action="<?php $_SERVER['PHP_SELF'];?>" method="post">				
 						<input type="hidden" name="song" value="tracknumber">
 						<input type="submit" name="submit" value="+" />					
 			</form>
-	</th><td><?php echo "<a href=\"song.php?track=" . $result_set[$i]['track']. "&id=".$result_set[$i]['id']."&artist_id=\"". $result_set[$i]['artist_id']."\">". $result_set[$i]['track']. "</a></td><td> " .$result_set[$i]['artist'].  "</td><td>".$result_set[$i]['album']."</td></tr>";
+	</th><td><?php echo "<a href=\"song.php?track=" . $result_set[$i]['track']. "&id=".$result_set[$i]['id']."&artist_id=\"". $result_set[$i]['artist_id']."\">". $result_set[$i]['track']. "</a></td><td> <a href=\"artist.php?artist=" .$result_set[$i]['artist']. "\">". $result_set[$i]['artist']."</a></td><td>  <a href=\"album.php?album=".$result_set[$i]['album']."\">". $result_set[$i]['album']. "</a></td></tr>";
 			$i++; 
 		}
 		echo "</table>";
@@ -91,3 +109,8 @@ $result_set = $stmt->fetchAll(PDO::FETCH_ASSOC); // fetch LIMIT 1
 	</div>
 		
 <?php include('footer.php'); ?>
+
+
+	
+
+	
